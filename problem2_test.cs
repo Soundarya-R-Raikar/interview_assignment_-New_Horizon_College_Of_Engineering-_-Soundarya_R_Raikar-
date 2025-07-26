60facde0ab2fdb574b37aba97a324ab255b04548
Soundarya-R-Raikar <raikarsonu4@gmail.com>
2025-07-26 17:58:33 +0530

    problem2_test.cs

--- problem2_test.cs
+++ problem2_test.cs
@@ -1,1 +1,20 @@
+using System;
+
+class PT2
+{
+    static void Main()
+    {
+        Test(new int[] { 0, 1, 0, 3, 12 });
+        Test(new int[] { 1, 2, 3, 4 });
+        Test(new int[] { 0, 0, 0, 0 });
+        Test(new int[] { 0, 1, 0, 4, 21 });
+    }
+
+    static void Test(int[] nums)
+    {
+        Console.Write("Before: [" + string.Join(", ", nums) + "] => ");
+        Pg2.ShiftingZeroes(nums);
+        Console.WriteLine("After: [" + string.Join(", ", nums) + "]");
+    }
+}
 
