bdc5fba9bfb832ec5686b3baa132db3916623194
Soundarya-R-Raikar <raikarsonu4@gmail.com>
2025-07-26 19:28:05 +0530

    problem1_test.cs

--- problem1_test.cs
+++ problem1_test.cs
@@ -4,10 +4,9 @@ class PT1
 {
     static void Main()
     {
-        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, 3, -1 }));    
-        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, -3, 4 }));    
-        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 })); 
-        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 0, 0, 0, 1 }));      
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, 3, -1 }));     
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, -3, 4 }));     
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));  
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 0, 0, 0, 1 }));     
     }
 }
-
