7aa5ffd7ff9dc1485daa271de736f3db46f379a9
Soundarya-R-Raikar <raikarsonu4@gmail.com>
2025-07-26 17:48:58 +0530

    problem1_test.cs

--- problem1_test.cs
+++ problem1_test.cs
@@ -4,12 +4,12 @@ class PT1
 {
     static void Main()
     {
-        Console.WriteLine(P1.DuplicateNumbersInArray(new int[] { 3, 2, 3, 9 }));      
-        Console.WriteLine(P1.DuplicateNumbersInArray(new int[] { 1, 2, 3, 4 }));      
-        Console.WriteLine(P1.DuplicateNumbersInArray(new int[] { 1, 2, 3, -1 }));    
-        Console.WriteLine(P1.DuplicateNumbersInArray(new int[] { 1, 2, -3, 4 }));    
-        Console.WriteLine(P1.DuplicateNumbersInArray(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 })); 
-        Console.WriteLine(P1.DuplicateNumbersInArray(new int[] { 0, 0, 0, 1 }));      
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 3, 2, 3, 9 }));      
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, 3, 4 }));      
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, 3, -1 }));    
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 2, -3, 4 }));    
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 })); 
+        Console.WriteLine(Pg1.DuplicateNumbersInArray(new int[] { 0, 0, 0, 1 }));      
     }
 }
 
