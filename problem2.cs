4fe1eb511050f50833b73e41758b2b3a54f6608a
Soundarya-R-Raikar <raikarsonu4@gmail.com>
2025-07-26 17:54:26 +0530

    problem2.cs

--- problem2.cs
+++ problem2.cs
@@ -1,1 +1,19 @@
+using System;
+
+public class Pg2
+{
+    public static void ShiftingZeroes(int[] nums)
+    {
+        int index = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+                nums[index++] = nums[i];
+        }
+
+        while (index < nums.Length)
+            nums[index++] = 0;
+    }
+}
 
