676dd8c537bed16a0ab6e689e125013079745c8a
Soundarya-R-Raikar <raikarsonu4@gmail.com>
2025-07-26 17:18:43 +0530

    problem1.cs

--- problem1.cs
+++ problem1.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class P1
+{
+    public static bool DuplicateNumberInArray(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (!seen.Add(num)) 
+                return true;
+        }
+        return false;
+    }
+}
 
